470f201e475fa149ab883c7b3001a9b6cf75e0b0
KooshaYeganeh <kooshakooshadv@gmail.com>
2025-01-27 20:55:59 +0330

    config File Added

---
+++ config.py
@@ -0,0 +1,14 @@
+
+## clamscan File Path
+SCANNCER = r'C:\Program Files\ClamAV\clamscan.exe'
+
+
+## Normal Apps
+winrar_url = 'https://dl2.soft98.ir/soft/w/WinRAR.7.01.exe?1735201498'
+notepad_url = 'https://dl2.soft98.ir/soft/n/Notepad.8.7.5.x64.rar?1735198965' 
+
+
+## Portable Apps
+
+portable_notepad = 'https://dl2.soft98.ir/soft/n/Notepad.8.6.7.Portable.rar?1735198990'
+portable_winrar = 'https://dl2.soft98.ir/soft/w/WinRAR.7.0.Portable.zip?1735201526'
